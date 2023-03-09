ef5399364f36f61d10b84bfbfa748670cb23c03c
Vishakh Prakash <vishakhpro2002@gmail.com>
2023-03-09 22:00:06 +0530

    FEAT: - Add img of different breeds to the src/assets folder - Create new function checkImage that predicts the image using mobilenet - Placed image in the Play component - Create button in the Play component - Create onClick function for the button and passing the function checkImage to it
    TEST:
    - Component only tested in mozilla firefox
    - The prediction of image not tested

--- src/Play.js
+++ src/Play.js
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
-
+import dogImg from "../src/assets/gd.jpeg"
 const Play = () => {
-    
     useEffect(() => {
+      
     //adding tensorflow-js
       const sptTf = document.createElement('script');
       sptTf.src = "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.0.1";
@@ -14,16 +14,31 @@ const Play = () => {
     //adding the elements to the document
         document.body.appendChild(sptTf);
         document.body.appendChild(sptMn);
+    
+    //get the image and pass it to 
       return () => {
         document.body.removeChild(sptTf);
         document.body.removeChild(sptMn)
       }
     }, [])
+
+    const  checkImage =  async () => {
+      const imag = document.getElementsByTagName('img');
+      const model = await window.mobilenet.load()
+      const pred = await model.classify(imag)
+      console.log(pred);
+      
+    }
+
     
     return ( 
-        <div className="bg-black h-screen">
-            <img src="{  }" alt="recognition question for children" className=""/>
-            <input type="text" className="bg-cyan-200 rounded-md flex mx-auto w-150" />
+        <div className="bg-black h-screen flex justify-center  items-center flex-col">
+
+            <img id = "image" src={ dogImg } alt="recognition question " className="mx-auto my-auto"/>
+            <div className="mb-56 w-screen mx-2 flex justify-center flex-col">
+            <input type="text" className="bg-cyan-200 rounded-md flex mx-auto w-1/2 p-2" placeholder="enter the breed" />
+            <button onClick = {() => {checkImage();}} className = "bg-cyan-700 hover:bg-cyan-500 hover:text-white mt-4 mx-auto p-4 rounded-full w-56" >submit</button>
+            </div>
         </div >
 
      );
