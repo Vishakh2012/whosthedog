b77ecce4d5f55d9fb249ef0e139f092808f5323e
Vishakh Prakash <vishakhpro2002@gmail.com>
2023-03-16 00:43:11 +0530

    FEAT: - Remove reduntant file robot.txt - Add import of all the images the asset folder - useCallback hook used on checkImage - setImg function implemented to add image randomly - img src initially set as gs
    TEST:
    - partial testing in mozilla firefox

--- src/Play.js
+++ src/Play.js
@@ -1,25 +1,34 @@
-import { useEffect, useState } from "react";
-import dogImg from "../src/assets/gd.jpeg"
+import { useEffect, useState , useCallback} from "react";
+import gd from "../src/assets/gd.jpeg"
+import gs from "../src/assets/gs.jpeg"
+import sh from "../src/assets/Siberian_Husky.jpeg"
 const Play =  () => {
+  
   const [Model, setModel] = useState(null);
   const [Guess, setGuess] = useState("");
   const [ans, setAns] = useState("");
 
-  const  checkImage =  async () => {
+
+  
+
+  const  checkImage =  useCallback( async () => {
       const imag = document.getElementsByTagName('img');
       const pred = await Model.classify(imag[0])
       const arrAns = pred[0].className.split(",")
       if (arrAns[0].toUpperCase() === ans.toUpperCase())
       {
-        console.log( ans +" is correct")
+        //console.log( ans +" is correct")
       }
       else
       {
-        console.log( ans + " is incorrect");
+        //console.log( ans + " is incorrect");
       }
       
-    }
+    }, [Model,ans]
+  )
     useEffect(() => {
+      
+      //console.log(img.src);
       
       const fetchTfnMn = async () => {
     //adding tensorflow-js
@@ -50,6 +59,7 @@ const Play =  () => {
       checkImage();
     }, [Guess,checkImage]);
 
+    
     const handleSubmit = async (e) => 
     {
       e.preventDefault();
@@ -58,14 +68,21 @@ const Play =  () => {
 
     }
 
-    
+    const setimg = async () => {
+      const arr = [gd,gs,sh]
+      const randomIndex = Math.floor(Math.random() * arr.length);
+      const selectedPicture = arr[randomIndex];
+      const img = document.getElementById("image")
+      img.src = selectedPicture
+    }
 
     
     return ( 
         <div className="bg-black h-screen flex justify-center  items-center flex-col">
 
-            <img id = "image" src={ dogImg } alt="recognition question " className="mx-auto my-auto"/>
-              <form className="w-screen mb-56 mx-2 flex justify-center flex-col" onSubmit={ async (e) => {await handleSubmit(e); checkImage() } }>
+            <img id = "image" src= { gs }   alt="recognition question " className="mx-auto my-auto"/>
+              <form className="w-screen mb-56 mx-2 flex justify-center flex-col" onSubmit={ async (e) => {await handleSubmit(e); checkImage() ; setimg()
+      } }>
             <input type="text" className="bg-cyan-200 rounded-full flex mx-auto w-1/2 py-2 px-4" placeholder="enter the breed" value = { Guess } onChange = {(e) => {setGuess(e.target.value); }}/>
             <button  className = "bg-cyan-700 hover:bg-cyan-500 hover:text-white mt-4 mx-auto p-4 rounded-full w-56" >submit</button>
             </form>
